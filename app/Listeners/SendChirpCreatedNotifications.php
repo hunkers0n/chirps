2f010ac5f8455bd9bba663cf14054ec191fb7cba
hunkers <hunkers.hilt0n@icloud.com>
2023-02-12 14:38:15 +0800

    Notify other users a new Chirp has been created.

--- app/Listeners/SendChirpCreatedNotifications.php
+++ app/Listeners/SendChirpCreatedNotifications.php
@@ -3,10 +3,12 @@
 namespace App\Listeners;
 
 use App\Events\ChirpCreated;
+use App\Models\User;
+use App\Notifications\NewChirp;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
-class SendChirpCreatedNotifications
+class SendChirpCreatedNotifications implements ShouldQueue
 {
     /**
      * Create the event listener.
@@ -26,6 +28,9 @@ class SendChirpCreatedNotifications
      */
     public function handle(ChirpCreated $event)
     {
-        //
+        # ->cursor is a database cursor that can avoid loading every user into memory at once.
+        foreach (User::whereNot('id', $event->chirp->user_id)->cursor() as $user) {
+            $user->notify(new NewChirp($event->chirp));
+        }
     }
 }
